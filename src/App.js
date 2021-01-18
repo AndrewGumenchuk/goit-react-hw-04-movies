e6435cacee439e764de0952d664ee0f814dba190
AndrewGumenchuk <64306832+AndrewGumenchuk@users.noreply.github.com>
2021-01-18 09:10:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { addBackToTop } from 'vanilla-back-to-top';
+
+import Container from './components/Container';
+import AppBar from './components/AppBar';
+import LoaderComponent from './components/LoaderComponent';
+
+const HomePage = lazy(() =>
+  import('./views/HomePage'),
+);
+
+const MoviesPage = lazy(() =>
+  import('./views/MoviesPage'),
+);
+
+const MovieDetailsPage = lazy(() =>
+  import(
+    './views/MovieDetailsPage'
+  ),
+);
+
+const NotFoundView = lazy(() =>
+  import('./views/NotFoundView'),
+);
 
 function App() {
+  useEffect(() => {
+    addBackToTop({
+      backgroundColor: '#fa7584',
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar />
+
+      <Suspense fallback={<LoaderComponent />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+
+          <Route path="/movies" exact>
+            <MoviesPage />
+          </Route>
+
+          <Route path="/movies/:slug">
+            <MovieDetailsPage />
+          </Route>
+
+          <Route>
+            <NotFoundView />
+          </Route>
+        </Switch>
+      </Suspense>
+
+      <ToastContainer autoClose={3700} position="bottom-center" />
+    </Container>
   );
 }
 
